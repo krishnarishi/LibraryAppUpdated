b955f4e520a00935975d66978272ef2f08b8cf00
krishnarishi <74538469+krishnarishi@users.noreply.github.com>
2020-12-27 10:57:50 +0530

    Update authorData.js

--- src/model/authorData.js
+++ src/model/authorData.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb+srv://userone:userone@ictakfiles.xnejz.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 
 const Schema = mongoose.Schema;
 
